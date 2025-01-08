8a39ab11fb772db9e095b1f5db282ce9d7a4340b
johnchuma <johnvchuma@gmail.com>
2025-01-08 11:14:58 +0300

    fix: order model

--- models/order.js
+++ models/order.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Order.hasManu(models.OrderedProduct);
+      Order.hasMany(models.OrderedProduct);
       // define association here
     }
   }
