391ccc430ae3172642ca7ed3d835a2948ce512b8
johnvchuma <johnvchuma@gmail.com>
2025-01-31 04:01:59 +0000

    fix: include shop

--- models/product.js
+++ models/product.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Product.belongsTo(models.Shop)
       Product.hasMany(models.PromotedProduct, {
         onDelete: "CASCADE",
         scope: true,
